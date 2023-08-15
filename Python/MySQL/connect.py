0194334e5b9c7bb1c7d549a83fce4b227f92e36b
skybluelee <107929903+skybluelee@users.noreply.github.com>
2023-08-15 22:08:28 +0900

    Update connect.py

--- Python/MySQL/connect.py
+++ Python/MySQL/connect.py
@@ -1,1 +1,17 @@
-zz
+# pip install mysql-connector-python
+import mysql.connector
+
+db_config = {
+    'host': 'localhost',
+    'user': '*',
+    'password': '*',
+    'database': '*'
+}
+
+conn = mysql.connector.connect(**db_config)
+cur = conn.cursor()
+
+sql = f"INSERT INTO dataknows.building_info VALUES ();"
+print(sql)
+cur.execute(sql)
+conn.commit()
